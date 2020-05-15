0d931c2131acf5ae6687b039e2b9a0cb5a02625c
Guinsly Mondesir <guinslym@gmail.com>
2020-05-14 20:21:24 -0400

    initial commit

---
+++ odesi_report_top_10_downloads/__init__.py
@@ -0,0 +1,5 @@
+__version__ = '0.1.0'
+from feedgen.feed import FeedGenerator
+#https://feedgen.kiesow.be/#installation
+
+
---
+++ tests/test_odesi_report_top_10_downloads.py
@@ -0,0 +1,5 @@
+from odesi_report_top_10_downloads import __version__
+
+
+def test_version():
+    assert __version__ == '0.1.0'
