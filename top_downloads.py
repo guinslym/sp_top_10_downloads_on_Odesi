effa504b31f1d6e236449fce11bf4438688bcb64
Guinsly Mondesir <guinslym@gmail.com>
2020-05-16 19:51:20 -0400

    readme

--- top_downloads.py
+++ top_downloads.py
@@ -14,11 +14,6 @@ import pymysql
 import pymysql.cursors
 from freezegun import freeze_time
 
-
-
-
-
-
 def get_top_downloads_on_odesi_for_last_month(year, month, qty=10):
     # pymysql query needs to be in one line
     sql = """SELECT S.survey_name AS 'Survey', S.survey_id AS 'Survey ID', SUM(counter) AS 'Download Count', LEFT(MAX(D1.date), 7) AS 'Month' FROM OdesiDailyAccess AS O1 INNER JOIN Dates AS D1 ON O1.date_id=D1.date_id LEFT JOIN Surveys AS S ON S.id=O1.survey_id WHERE D1.date LIKE '{0}-{1}-%' AND O1.mode_id=5 AND O1.execute_download=True GROUP BY O1.survey_id ORDER BY SUM(counter) DESC LIMIT {2};""".format(year, f'{month:02d}', qty)
