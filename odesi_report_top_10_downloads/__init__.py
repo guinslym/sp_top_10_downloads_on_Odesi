cc4e99d46ffd28d42acbf05f22db3b20e6b62ce6
Guinsly Mondesir <guinslym@gmail.com>
2020-05-16 12:29:20 -0400

    everything works

--- odesi_report_top_10_downloads/__init__.py
+++ odesi_report_top_10_downloads/__init__.py
@@ -7,26 +7,35 @@ from dateparser import parse
 import pymysql
 import pymysql.cursors
 
+from freezegun import freeze_time
+import datetime
+from decimal import *
+
+import json
+
 
 from environs import Env
 env = Env()
 # Read .env into os.environ
 env.read_env()
 
+
 #Constant
 # environment = env("ENVIRONMENT", "STAGING")
 # if environment == "STAGING":
 
-this_date = parse('1 month ago')
-month = this_date.month
-year = str(this_date.year)
+with freeze_time("2020-03-14"):
+    this_date = parse('1 month ago')
+    month = this_date.month
+    year = str(this_date.year)
 
-
-
-sql2 = """SELECT left(S.survey_name, 100 )AS 'Survey', left(S.survey_id, 100) AS 'Survey ID', SUM(counter) AS 'Download Count', LEFT(MAX(D1.date), 7) AS 'Month' FROM OdesiDailyAccess AS O1 INNER JOIN Dates AS D1 ON O1.date_id=D1.date_id LEFT JOIN Surveys AS S ON S.id=O1.survey_id WHERE D1.date LIKE '{0}-{1}-%' AND O1.mode_id=5 AND O1.execute_download=True GROUP BY O1.survey_id ORDER BY SUM(counter) DESC LIMIT 10;""".format(year, f'{month:02d}')
 
 #for pymysql query needs to be on one line
-sql2 = """SELECT left(S.survey_name, 40 )AS 'Survey', left(S.survey_id, 20) AS 'Survey ID', SUM(counter) AS 'Download Count', LEFT(MAX(D1.date), 7) AS 'Month' FROM OdesiDailyAccess AS O1 INNER JOIN Dates AS D1 ON O1.date_id=D1.date_id LEFT JOIN Surveys AS S ON S.id=O1.survey_id WHERE D1.date LIKE '2020-02-%' AND O1.mode_id=5 AND O1.execute_download=True GROUP BY O1.survey_id ORDER BY SUM(counter) DESC LIMIT 10;"""
+sql2 = """SELECT left(S.survey_name, 100 )AS 'Survey', left(S.survey_id, 100) AS 'Survey ID', SUM(counter) AS 'Download Count', LEFT(MAX(D1.date), 7) AS 'Month' FROM OdesiDailyAccess AS O1 INNER JOIN Dates AS D1 ON O1.date_id=D1.date_id LEFT JOIN Surveys AS S ON S.id=O1.survey_id WHERE D1.date LIKE '2020-02-%' AND O1.mode_id=5 AND O1.execute_download=True GROUP BY O1.survey_id ORDER BY SUM(counter) DESC LIMIT 10;"""
+
+#for pymysql query needs to be on one line
+sql2 = """SELECT S.survey_name AS 'Survey', S.survey_id AS 'Survey ID', SUM(counter) AS 'Download Count', LEFT(MAX(D1.date), 7) AS 'Month' FROM OdesiDailyAccess AS O1 INNER JOIN Dates AS D1 ON O1.date_id=D1.date_id LEFT JOIN Surveys AS S ON S.id=O1.survey_id WHERE D1.date LIKE '{0}-{1}-%' AND O1.mode_id=5 AND O1.execute_download=True GROUP BY O1.survey_id ORDER BY SUM(counter) DESC LIMIT 10;""".format(year, f'{month:02d}')
+
 
 # connection = pymysql.connect(
 #     user=env("USER"),
@@ -44,5 +53,9 @@ with connection.cursor() as cursor:
     result = cursor.fetchall()
     print(result)
 
+for data in result:
+    data['Download Count'] = int(data['Download Count'])
 
+with open('downloads.json', 'w', encoding='utf-8') as f:
+    json.dump(result, f, ensure_ascii=False, indent=4)
 
