1a553398f83ae9e487e95dde27f6333655b4c97c
Guinsly Mondesir <guinslym@gmail.com>
2020-05-19 23:20:25 -0400

    testing

--- tests/test_odesi_report.py
+++ tests/test_odesi_report.py
@@ -13,3 +13,27 @@ from sp_utils import save_result_in_json_file
 
 def test_version():
     assert __version__ == '0.1.0'
+
+class TestTopDownloads(object):
+    def test_get_date(self):
+        pass
+    def test_get_top_10_odesi_downloads_for_this_month(self):
+        pass
+
+class TestOdesiQueries:
+    def test_sql_query_top_downloads_on_odesi_for_last_month(self):
+        pass
+
+class TestOdesiQueryHelper(object):
+    def test_execute_this_query():
+        assert True
+    def test_get_connection():
+        assert True
+    def test_ clean_decimal_data():
+        assert True
+
+class SPUtils():
+    def test_read_json_file():
+        pass
+    def test_save_result_in_json_file():
+        pass
